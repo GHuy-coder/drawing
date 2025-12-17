d63e6e3bb2dfc88e7e042fc1a842b399da2491c9
GiaHuy <giahuyluong609@gmail.com>
2025-12-17 20:37:30 +0700

    update2

--- main.py
+++ main.py
@@ -23,14 +23,25 @@ def chon_anh(selected_img):
     current_img_path =f"birds/{selected_img}"
 
     image = Image.open(current_img_path)
-    size = (500, 500)
+    size = (450, 450)
     resized_image = image.resize(size)
     resized_image.save(current_img_path)
     draw.image(0,0,resized_image)
 
-app = App(title="APP", width=900, height=820)
+def dchuyen():
+    global x, y
+    x = S_x.value
+    y = S_y.value
+    draw.clear()
+    chon_anh(lst.value)
+    draw.text(
+        x,y, textbox.value, color= combo.value, font= group.value, size= slide.value
+    )
+    
+
+app = App(title="APP", width=900, height=900)
 box = Box(app, layout="grid", width=450, height=250, border=True)
-box.bg = ""
+box.bg = "#F0FFFF"
 Text(box, text="Enter text: ", size =15, grid=[0,0])
 textbox = TextBox(box, width=25, grid=[1,0])
 textbox.text_size = 15
@@ -44,17 +55,27 @@ group.text_size = 15
 group.bg = "#F2CD5D"
 Text(box, text="Text Size: ", size =15, grid=[0,3])
 slide = Slider(box, start=10,end= 100, width=250, grid=[1,3])
+ano_box = Box(app, width=450, height=130, layout="grid")
+Text(ano_box, text="Select meme sticker: ", grid=[0,0])
+Text(ano_box, text="X", grid=[0,1])
+Text(ano_box, text="Y", grid=[0,2])
+meme = Combo(ano_box, options=["star", "rectangle", "triangle"], grid=[1,0])
+meme.text_size= 13
+S_x = Slider(ano_box, start=0, end=450, width=200, grid=[1,1], command=dchuyen)
+S_y = Slider(ano_box, start=0, end=450, width=200, grid=[1,2], command=dchuyen)
+
+
 
 
 push = PushButton(app, text="Update", width=6, command=upd_meme)
 push.text_size = 15
 push.bg="#FF00FF"
 
-box1 = Box(app, layout="grid", width=750, height=600)
+box1 = Box(app, layout="grid", width=700, height=600)
 lst = ListBox(box1, items=load_img(), grid=[0,0], command=chon_anh)
 lst.text_size=15
 lst.bg= "#4169E1"
-draw = Drawing(box1, grid=[1, 0], width=500, height=500)
+draw = Drawing(box1, grid=[1, 0], width=450, height=450)
 
 
 app.display()
