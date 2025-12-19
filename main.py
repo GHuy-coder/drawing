b8dd48bdfb90cc6cfdbfc1fd76256e25bb01b304
GiaHuy <giahuyluong609@gmail.com>
2025-12-19 16:45:21 +0700

    Ugd2.5

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def upd_meme():
     global lst
     chon_anh(lst.value)
     draw.text(
-        0,0, textbox.value, color= combo.value, font= group.value, size= slide.value
+        0,0, chu.value, color= mau.value, font= phong.value, size= co.value
     )
+    if meme.value == 'square':
+        draw.rectangle(50,50,120,120,color=mau.value)
 def load_img():
     
     img_file = []
@@ -35,7 +37,7 @@ def dchuyen():
     draw.clear()
     chon_anh(lst.value)
     draw.text(
-        x,y, textbox.value, color= combo.value, font= group.value, size= slide.value
+        x,y, chu.value, color= mau.value, font= phong.value, size= co.value
     )
     
 
@@ -43,23 +45,23 @@ app = App(title="APP", width=900, height=900)
 box = Box(app, layout="grid", width=450, height=250, border=True)
 box.bg = "#F0FFFF"
 Text(box, text="Enter text: ", size =15, grid=[0,0])
-textbox = TextBox(box, width=25, grid=[1,0])
-textbox.text_size = 15
+chu = TextBox(box, width=25, grid=[1,0])
+chu.text_size = 15
 Text(box, text="Select text color: ", size =15, grid=[0,1])
-combo = Combo(box, options=["black", "Crimson", "blue"], grid=[1,1])
-combo.text_size = 15
-combo.bg = "#28C2FF"
+mau = Combo(box, options=["black", "Crimson", "blue"], grid=[1,1])
+mau.text_size = 15
+mau.bg = "#28C2FF"
 Text(box, text="Select font: ", size =15, grid=[0,2])
-group = ButtonGroup(box, options=["Arial", "Times New Roman", "Courier"], grid=[1,2])
-group.text_size = 15
-group.bg = "#F2CD5D"
+phong = ButtonGroup(box, options=["Arial", "Times New Roman", "Courier"], grid=[1,2])
+phong.text_size = 15
+phong.bg = "#F2CD5D"
 Text(box, text="Text Size: ", size =15, grid=[0,3])
-slide = Slider(box, start=10,end= 100, width=250, grid=[1,3])
+co = Slider(box, start=10,end= 100, width=250, grid=[1,3])
 ano_box = Box(app, width=450, height=130, layout="grid")
 Text(ano_box, text="Select meme sticker: ", grid=[0,0])
 Text(ano_box, text="X", grid=[0,1])
 Text(ano_box, text="Y", grid=[0,2])
-meme = Combo(ano_box, options=["star", "rectangle", "triangle"], grid=[1,0])
+meme = Combo(ano_box, options=["star", "square", "triangle"], grid=[1,0])
 meme.text_size= 13
 S_x = Slider(ano_box, start=0, end=450, width=200, grid=[1,1], command=dchuyen)
 S_y = Slider(ano_box, start=0, end=450, width=200, grid=[1,2], command=dchuyen)
